8c7b66b5f926d0c81a718dccdd16914356327c9f
rahul kumar <rkrahul8181@gmail.com>
2023-03-01 13:41:56 +0530

    timer

--- React/interviewPractice/src/App.jsx
+++ React/interviewPractice/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
 
   return (
     <div className='center'>
-      <Practice />
+      {/* <Practice /> */}
+      <Timer />
     </div>
   )
 }
--- React/interviewPractice/src/components/Timer.jsx
+++ React/interviewPractice/src/components/Timer.jsx
@@ -1,29 +1,87 @@
-import React, { useEffect,useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Timer = () => {
-    const [timer,setTimer] = useState(10);
-    useEffect(()=>{
-        counterFun()
-    },[])
+    const [value, setValue] = useState("");
+    const timerRef = useRef();
+    const [show, setShow] = useState(false)
 
-    const counterFun = ()=>{
-        setInterval(()=>{
-            setTimer(prep=>{
-                if(timer<=0){
-                    return 10;
-                }else{
-                    return prep-1
-                }
-            })
-        },1000)
+    useEffect(() => {
+
+        return () => {
+            if (timerRef.current && !show)
+                clearInterval(timerRef.current)
+        }
+    }, [show])
+
+    const handleChange = (e) => {
+        if (e.target.value.length <= 2) {
+            setValue(e.target.value)
+        }
+        // console.log(typeof e.target.value)
+        // console.log(e.target.maxLength)
+
     }
 
-  return (
-    <div>
-       
-       {timer}
-    </div>
-  )
+    const handleStart = () => {
+
+        if (timerRef.current) return;
+
+        timerRef.current = setInterval(() => {
+
+            setValue((prep => {
+                if (prep <= 1) {
+                    clearInterval(timerRef.current);
+                    return 0
+                }
+                return prep - 1
+            }));
+            console.log("inside handleStartfunction", value)
+        }, 1000)
+    }
+
+    const handleReset = () => {
+        if (timerRef.current) {
+            setValue(0);
+            clearInterval(timerRef.current)
+            timerRef.current = null
+        } else if (value != 0) setValue(0)
+    }
+
+    const handleStop = () => {
+        if (timerRef.current)
+            clearInterval(timerRef.current)
+        timerRef.current = null
+    }
+
+    console.log("outside", value)
+    return (
+        <div id='timer__component'>
+
+            {!show && <>
+                <div className='timer__input-number'>
+                    <label>Number</label>
+                    <input
+                        maxLength={"2"}
+                        type={"number"}
+                        value={value}
+                        onChange={handleChange}
+                        placeholder="00"
+                    />
+                </div>
+                <div className='timer__buttons'>
+                    <button onClick={handleStart} >Start</button>
+                    <button onClick={handleReset} >Reset</button>
+                    <button onClick={handleStop} >Stop</button>
+                </div>
+            </>}
+            <h1 style={{ cursor: "pointer", background: "purple", padding: "5px 8px" }}
+                onClick={() => setShow(!show)}
+            >
+                {show ? "SHOW" : "HIDE"}
+            </h1>
+        </div>
+
+    )
 }
 
 export default Timer
