f64cf04de209d02c3a701f03fee78a4e407be224
rahul kumar <rkrahul8181@gmail.com>
2023-04-11 14:44:50 +0530

    Output based 4 ExpressJs quesiton

--- Backend/express/index.js
+++ Backend/express/index.js
@@ -1,26 +1,39 @@
 const app = require('express')();
 const PORT = 8080;
 
-const Middlware1 = (req,res,next)=>{
-    console.log("MIddleware1");
-    next()
-}
-const Middlware2 = (req,res,next)=>{
-    console.log("MIddleware2");
-    next()
-}
-const findTheReq = (req,res,next)=>{
-    console.log(req.url);
+// const Middlware1 = (req,res,next)=>{
+//     console.log("MIddleware1");
+//     next()
+// }
+// const Middlware2 = (req,res,next)=>{
+//     console.log("MIddleware2");
+//     next()
+// }
+// const findTheReq = (req,res,next)=>{
+//     console.log(req.url);
+//     next();
+// }
+// const Middlware3 = (req,res,next)=>{
+//     console.log("MIddleware3");
+//     next()
+// }
+
+// app.use(Middlware1);
+app.use((req,res,next)=>{
+    console.log("MIddleware1")
     next();
-}
-const Middlware3 = (req,res,next)=>{
-    console.log("MIddleware3");
     next()
-}
+});
+app.use((req,res,next)=>{
+    console.log("MiddlwWare2")
+    next()
+})
+app.use((req,res,next)=>{
+    console.log("MiddlwWare3")
+    next()
+})
 
-app.use(Middlware1);
-
-app.get('/',Middlware2,(req,res)=>{
+app.get('/',(req,res)=>{
     console.log("I am inise");
     res.send("HelloIs it working")
 });
