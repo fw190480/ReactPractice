d0ee0c45590363ef8652d67349ac7ef5ef9b98ca
rahul kumar <rkrahul8181@gmail.com>
2023-04-11 14:38:50 +0530

    Output based Question Express 1

---
+++ Backend/express/index.js
@@ -0,0 +1,28 @@
+const app = require('express')();
+const PORT = 8080;
+
+const Middlware1 = (req,res,next)=>{
+    console.log("MIddleware1");
+    next()
+}
+const Middlware2 = (req,res,next)=>{
+    console.log("MIddleware2");
+    next()
+}
+const findTheReq = (req,res,next)=>{
+    console.log(req.url);
+}
+// const Middlware3 = (req,res,next)=>{
+//     console.log("MIddleware3");
+//     next()
+// }
+
+app.use(Middlware1,findTheReq);
+
+app.get('/',Middlware2,(req,res)=>{
+    console.log("I am inise");
+    res.send("HelloIs it working")
+});
+
+
+app.listen(PORT);
